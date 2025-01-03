13ac4593510d850a30a66b6add2626b11d3e858f
Shobhit <parmarshobhit91@gmail.com>
2025-01-04 00:03:35 +0530

    Strings in javascript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+let str1 = "hello"
+let str2 = " user"
+let age = 22
+
+// console.log(str1 + str2 + age); this syntax is not so good please try to ignore this
+
+console.log(`Hello my name is ${str1} and my surname is ${str2}`)
+
+const gameName = new String("shobhit")
+
+// string methods like trim, toUpperCase, toLowerCase, split, length, charAt, indexOf etc.
