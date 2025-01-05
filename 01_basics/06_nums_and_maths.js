13e4e4142ad9f2dd5e1635e0439db7a4aef54cbf
Shobhit <parmarshobhit91@gmail.com>
2025-01-05 11:54:16 +0530

    Numbers and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 4000
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const newNumber = 1123.8966
+
+// console.log(newNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ---------------- Maths -------------------
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.min(4,3,5,6))
+// console.log(Math.max(4,3,5,6))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor((Math.random()*10))+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
